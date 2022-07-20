72cf17df1ac63eba14f4fd83891ea21c15094a38
anqiclaire <40797584+anqiclaire@users.noreply.github.com>
2022-07-20 17:52:35 -0400

    include poisson ratio
    Effective poisson ratio is required for plain strain modulus calculation.

--- readODB.py
+++ readODB.py
@@ -6,8 +6,13 @@ from odbAccess import openOdb
 from abaqusConstants import NODAL
 
 # abaqus trims sys.argv until after "python"
-this_script, name, displacement = sys.argv
+if len(sys.argv) < 4:
+	this_script, name, displacement = sys.argv
+	poisson = 0
+else:
+	this_script, name, displacement, poisson = sys.argv
 displacement = float(displacement)
+poisson = float(poisson)
 tsv = csv.writer(open(name+'-youngs.tsv', 'wb'), dialect=csv.excel_tab)
 tsv.writerow(('frequency (Hz)', 'E_Real (Pa)', 'E_Imag (Pa)'))
 odb = openOdb(name+'.odb', readOnly=True)
@@ -20,6 +25,6 @@ for frame in odb.steps['STEP-1'].frames:
 	E_Real = 0
 	E_Imag = 0
 	for v in RF.values:
-		E_Real += v.data[0] / displacement
-		E_Imag += v.conjugateData[0] / displacement
+		E_Real += v.data[0] / displacement * (1 - poisson**2)
+		E_Imag += v.conjugateData[0] / displacement * (1 - poisson**2)
 	tsv.writerow((frequency, E_Real, E_Imag))
