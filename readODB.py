9329cb3a4066c8ebc08ac135dc7207d63e236c61
richardsheridan <richard.sheridan@gmail.com>
2022-12-20 17:10:45 -0500

    use numpy to write 2D/3D data

--- readODB.py
+++ readODB.py
@@ -1,29 +1,38 @@
 """Extract viscoelastic response from abaqus output ODB to TSV"""
 import sys
-import csv
+
+import numpy as np
 
 from odbAccess import openOdb
 from abaqusConstants import NODAL
 
 # abaqus trims sys.argv until after "python"
 this_script, name, drive_nodeset = sys.argv
-tsv = csv.writer(open(name+'-reaction-force.tsv', 'wb'), dialect=csv.excel_tab)
 # NOTE: This code cannot know what the proper units for frequency or RF are,
 #       as it depends on the interpretation of the inputs in the INP file.
 # TODO: add an extra column for step number?
-tsv.writerow(('frequency', 'RF_Real', 'RF_Imag'))
 odb = openOdb(name+'.odb', readOnly=True)
 drive_nset = odb.rootAssembly.instances['PART-1-1'].nodeSets[drive_nodeset.upper()]
+step_results = []
 for step in odb.steps.values():
 	for frame in step.frames:
 		frequency = frame.frameValue
 		if frequency == 0:
 			continue
 		RF = frame.fieldOutputs['RF'].getSubset(region=drive_nset, position=NODAL)
-		RF_Real = 0
-		RF_Imag = 0
+		RF_Real = np.zeros_like(RF.values[0].data)
+		RF_Imag = RF_Real.copy()
 		for v in RF.values:
-			RF_Real += v.data[0]
+			RF_Real += v.data
 			if v.conjugateData is not None:  # Dynamic data only
-				RF_Imag += v.conjugateData[0]
-		tsv.writerow((frequency, RF_Real, RF_Imag))
+				RF_Imag += v.conjugateData
+		step_results.append(np.concatenate(([frequency], RF_Real, RF_Imag)))
+
+header = ["frequency"]
+for i in range(1, 1+len(RF_Real)):
+	header.append("RF_Real"+str(i))
+for i in range(1, 1+len(RF_Imag)):
+	header.append("RF_Imag"+str(i))
+header = "\t".join(header)
+
+np.savetxt(name+'-reaction-force.tsv', step_results, delimiter="\t", comments="", header=header)
